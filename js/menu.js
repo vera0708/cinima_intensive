07e025963333751a1cdde49204e4f661f7f8724f
Vera Deeva <verasdee@gmail.com>
2023-08-31 21:38:37 +0700

    feat: made burger menu

---
+++ js/menu.js
@@ -0,0 +1,26 @@
+const openMenu = (nav, active) => {
+    nav.classList.add(active);
+};
+
+const closenMenu = (nav, active) => {
+    nav.classList.remove(active);
+}
+
+const slideMenu = ({ openBtn, menu, classActiveMenu, closeTrigger }) => {
+    const burgerBtn = document.querySelector(openBtn);
+    const navigation = document.querySelector(menu);
+    const navigationClose = document.querySelectorAll(closeTrigger);
+    console.log(navigationClose);
+
+    burgerBtn.addEventListener('click', () => {
+        openMenu(navigation, classActiveMenu);
+    });
+
+    navigationClose.forEach((item) => {
+        item.addEventListener('click', () => {
+            closenMenu(navigation, classActiveMenu);
+        });
+    });
+};
+
+export default slideMenu;
